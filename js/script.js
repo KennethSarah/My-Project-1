395e497b930ca004048e25c2d21071ea44b64106
Kenneth Sarah <kennethnifemi@gmail.com>
2023-09-23 14:23:52 +0100

    project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+   // Add functionality to buttons
+   document.querySelector('.register').addEventListener('click', function() {
+    alert('Register button clicked');
+});
+
+document.querySelector('.login').addEventListener('click', function() {
+    alert('Login button clicked');
+});
